d1abed8a1d0acaad1a3eb361a0924ec06f97d924
Toomas Marjapuu <toomashaide@gmail.com>
2026-07-16 20:53:05 +0800

    Feat: Add favicon

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -6,6 +6,21 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Sloppify",
   description: "Sloppify, the hottest URL shortener on the web",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 type RootLayoutProps = Readonly<{
